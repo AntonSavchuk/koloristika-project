e556fb88f116da1836412174c1214239d185e930
AntonSavchuk <savchuk.anton.s@gmail.com>
2025-02-07 14:47:59 +0300

    services.views.product_price html added

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 
 from . import views
+from services import views as serv_views
 
 app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('prices/', serv_views.product_price, name='price'),
 ]
