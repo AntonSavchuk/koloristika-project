56183c31b46978f5b94c47e3f336f23fa11b1e68
AntonSavchuk <savchuk.anton.s@gmail.com>
2025-02-12 17:56:54 +0300

    burger menu was changed by rolling back styles

--- koloristika/settings.py
+++ koloristika/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "koloristika.urls"
